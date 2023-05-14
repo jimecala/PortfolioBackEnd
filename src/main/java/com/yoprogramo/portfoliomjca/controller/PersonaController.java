2269be616b0b7ae6defe7d11f6f3726f1b2141e6
Jimena <jimecala@gmail.com>
2023-05-14 16:56:04 -0300

    Configuración CORS

--- src/main/java/com/yoprogramo/portfoliomjca/controller/PersonaController.java
+++ src/main/java/com/yoprogramo/portfoliomjca/controller/PersonaController.java
@@ -4,7 +4,6 @@ import com.yoprogramo.portfoliomjca.model.Persona;
 import com.yoprogramo.portfoliomjca.service.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,8 +14,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-//@RequestMapping(path = "/api/datos-personales")
-//@CrossOrigin(origins = "https://portfoliomjca.onrender.com")
 public class PersonaController {
     
     @Autowired
